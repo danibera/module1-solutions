e89862560a7a0c39789c167bb63d23af6ffe5925
Dani <danielle@coursera-something.com>
2016-11-18 14:23:10 -0300

    Next version of the web files.

---
+++ scripts/app.js
@@ -0,0 +1,48 @@
+(function () {
+  angular.module("LunchCheck",[])
+  .controller("LunchCheckController",function($scope) {
+    $scope.name="";
+
+    $scope.checkItens=function () {
+
+      var names = $scope.name;
+
+      var result = names.split(',');
+      var totallength = result.length;
+      var lenEmpty = 0;
+
+      for (var i = 0; i < result.length; i++) {
+        console.log(result[i]);
+        if (result[i] != null && result[i] == " " && result[i].length == 1) {
+          lenEmpty ++;
+
+        }
+      }
+      var correctLen = totallength - lenEmpty;
+
+      console.log("emptylen",lenEmpty);
+      console.log("corroect",correctLen);
+      console.log(result);
+
+      if (correctLen == 1 && result == "") {
+        $scope.isempty = "enter a value first";
+        $scope.color = {"color":"red"};
+        $scope.bordercolor = {"border-color":"red"};
+      }
+      else if (correctLen <= 3) {
+        $scope.isempty = "enjoy.!";
+        $scope.color = {"color":"green"};
+        $scope.bordercolor = {"border-color":"green"};
+      }
+      else if (correctLen >= 4) {
+        $scope.isempty = "Too much.!"
+        $scope.color = {"color":"green"};
+        $scope.bordercolor = {"border-color":"green"};
+      }
+      else{
+        $scope.isempty = "";
+      }
+    };
+
+  })
+})();
